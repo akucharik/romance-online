c38bbf051d8c971ea54640006699e00d9d30770f
Adam Kucharik <Adam@Adams-MacBook-Air.local>
2014-08-29 00:22:28 -0400

    Update main menu screen
    Restructure views, add comparator to characters

---
+++ js/game/views/home.js
@@ -0,0 +1,57 @@
+define([
+	'backbone',
+    'jquery',
+    'constants',
+    'views/characters',
+    'views/mainMenu'
+], function(
+    Backbone,
+    $,
+    constants,
+    CharactersView,
+    MainMenuView
+) {
+
+    var HomeView = Backbone.View.extend({
+        initialize: function () {
+            this.activeView = null;
+            
+            this.listenTo(this.model, 'change:state', this.render);
+            this.render();
+		},
+        
+        render: function () {
+            if (this.activeView) {
+                this.activeView.remove();
+            }
+            
+            switch (this.model.get('state')) {
+                case constants.home.state.MAIN_MENU:
+                    this.activeView = new MainMenuView({
+                        className: 'main-menu-view',
+                        id: 'mainMenuView',
+                        model: this.model,
+                        template: '#mainMenuTemplate'
+                    });
+                    break;
+                
+                case constants.home.state.GAMES:
+                    console.log('view saved games!');    
+                    break;
+                    
+                case constants.home.state.CHARACTERS:
+                    this.activeView = new CharactersView({
+                        className: 'characters-view',
+                        id: 'charactersView',
+                        model: this.model,
+                        template: '#charactersTemplate'
+                    });
+                    break;
+            }
+            
+            this.$el.append(this.activeView.el);
+        }
+    });
+        
+	return HomeView;
+});
